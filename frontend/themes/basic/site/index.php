0742f3b2cfb794991995d44eb71f5a9a2ba1fc12
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2018-09-16 11:19:51 +0700

    direct link to setkab project in site index

--- frontend/themes/basic/site/index.php
+++ frontend/themes/basic/site/index.php
@@ -29,7 +29,8 @@ if (Yii::$app->user->isGuest) {
     <div style='text-align:center;background-color:#F9F9F9;'><img align='center' src ='/images/front.png'></div>";
 } 
                       else {
-                        echo "<br/>".Html::a(Yii::t('app', 'Select Project'), ['projects/project/select'], ['class' => 'btn btn-lg btn-success']);
+			echo '<div class="col-sm-3" style="margin-top: 3rem;"><a href="http://projects.ppsdm.com/index.php/projects/project/dashboard?id=5"><img class="img-thumbnail" src="http://setkab.go.id/wp-content/uploads/2016/02/garuda.jpg"><h3>Sekretariat Kabinet</h3></a></div>';
+                        //echo "<br/>".Html::a(Yii::t('app', 'Select Project'), ['projects/project/select'], ['class' => 'btn btn-lg btn-success btn-small']);
                        // echo "<div style='align:center;background-color:#F9F9F9;'><img align='center' src ='/images/front.png'></div>";
                         
                       }
